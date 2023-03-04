63639bade68d485d391bcd9fb732602f545a9674
Arpad TOTH <arpad.toth@zeiss.com>
2023-03-04 08:22:42 +0100

    client started with require error

--- se-client/src/index.ts
+++ se-client/src/index.ts
@@ -1,10 +1,27 @@
-import { fetchFileList } from "./functions/call-api";
+console.log("yey");
+import tslib from 'tslib';
+import axios from 'axios';
+
+// import { fetchFileList } from "./functions/call-api";
 
 const baseUrl = "http://localhost:3000/file";
-const main = async () => {    
-  console.log(baseUrl);
-  fetchFileList(baseUrl)
-}
+
+function main() {
+  console.log(tslib);
+  fetchFileList(baseUrl);
+};
+
+
+const fetchFileList = async (url: string) => {
+  let promise = axios
+    .get(url + "/file", { responseType: "text" })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => console.log(err));
+  return promise;
+};
+
+
 
 main();
-
